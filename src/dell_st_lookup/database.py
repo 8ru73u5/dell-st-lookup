2aec900086a4641a5118ade9485a2f261b1b8ffb
8ru73u5 <brute.us@protonmail.com>
2023-07-09 15:48:00 +0200

    Changed warranty variables in DellDevice class to be optionals for cases when there's no warranty. Updated caching class accordingly and added cache protocol class as well as dummy cache mechanism

--- src/dell_st_lookup/database.py
+++ src/dell_st_lookup/database.py
@@ -1,25 +1,38 @@
 import sqlite3
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Protocol
 
 from dell_st_lookup.config import Config
 from dell_st_lookup.device import DellDevice
 
 
-class DeviceCache:
+class DellDeviceCache(Protocol):
+    def open(self) -> None:
+        ...
+
+    def get_device(self, service_tag: str) -> Optional[DellDevice]:
+        ...
+
+    def insert_device(self, device: DellDevice) -> None:
+        ...
+
+    def close(self) -> None:
+        ...
+
+
+class SQLiteDeviceCache:
     _DATE_FORMAT = '%d %b %Y'
 
     def __init__(self, config: Config) -> None:
         self.connection = sqlite3.Connection(config.database_path)
         self.connection.row_factory = sqlite3.Row
-        self._init_database()
 
-    def _init_database(self) -> None:
+    def open(self) -> None:
         query = '''
             CREATE TABLE IF NOT EXISTS devices (
-                service_tag TEXT PRIMARY KEY,
-                device_type TEXT,
-                name TEXT,
+                service_tag TEXT PRIMARY KEY NOT NULL,
+                device_type TEXT NOT NULL,
+                name TEXT NOT NULL,
                 warranty_type TEXT,
                 warranty_expiration_date TEXT
             );
@@ -39,7 +52,14 @@ class DeviceCache:
         if not device:
             return None
 
-        expiration_date = datetime.strptime(device['warranty_expiration_date'], self._DATE_FORMAT)
+        expiration_date_from_db: Optional[str] = device['warranty_expiration_date']
+        if expiration_date_from_db is not None:
+            expiration_date = datetime.strptime(
+                device['warranty_expiration_date'], self._DATE_FORMAT
+            )
+        else:
+            expiration_date = None
+
         return DellDevice(
             device_type=device['device_type'],
             name=device['name'],
@@ -57,16 +77,37 @@ class DeviceCache:
             ) ON CONFLICT DO NOTHING;
         '''
 
-        params = ({
-            'service_tag': device.service_tag.upper(),
-            'device_type': device.device_type,
-            'name': device.name,
-            'warranty_type': device.warranty_type,
-            'warranty_expiration_date': device.warranty_expiration_date.strftime(self._DATE_FORMAT)
-        },)
+        if device.has_warranty():
+            expiration_date = device.warranty_expiration_date.strftime(self._DATE_FORMAT)
+        else:
+            expiration_date = None
+
+        params = (
+            {
+                'service_tag': device.service_tag.upper(),
+                'device_type': device.device_type,
+                'name': device.name,
+                'warranty_type': device.warranty_type,
+                'warranty_expiration_date': expiration_date
+            },
+        )
 
         with self.connection:
             self.connection.executemany(query, params)
 
-    def quit(self) -> None:
+    def close(self) -> None:
         self.connection.close()
+
+
+class DummyDeviceCache:
+    def open(self) -> None:
+        pass
+
+    def get_device(self, service_tag: str) -> Optional[DellDevice]:
+        return None
+
+    def insert_device(self, device: DellDevice) -> None:
+        pass
+
+    def close(self) -> None:
+        pass
--- src/dell_st_lookup/device.py
+++ src/dell_st_lookup/device.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 
 
 @dataclass(frozen=True, slots=True)
@@ -7,8 +8,8 @@ class DellDevice:
     device_type: str
     name: str
     service_tag: str
-    warranty_type: str
-    warranty_expiration_date: datetime
+    warranty_type: Optional[str]
+    warranty_expiration_date: Optional[datetime]
 
     def __hash__(self) -> int:
         return hash(self.service_tag)
@@ -20,18 +21,37 @@ class DellDevice:
         return self.service_tag == other.service_tag
 
     def __str__(self) -> str:
-        remaining_days = abs(self.get_days_until_warranty_expires())
-        warranty_date_text = self.warranty_expiration_date.strftime('%d %b %Y')
+        if self.has_warranty():
+            remaining_days = abs(self.get_days_until_warranty_expires())
+            warranty_date_text = self.warranty_expiration_date.strftime('%d %b %Y')
 
-        if self.has_warranty_expired():
-            warranty_text = f'warranty expired {remaining_days} days ago - {warranty_date_text}'
+            if self.has_warranty_expired():
+                warranty_text = f'warranty expired {remaining_days} days ago' \
+                                f' - {warranty_date_text}'
+            else:
+                warranty_text = f'under warranty for {remaining_days} more days' \
+                                f' - {warranty_date_text}'
         else:
-            warranty_text = f'under warranty for {remaining_days} more days - {warranty_date_text}'
+            warranty_text = 'no warranty information'
 
-        return f'{self.device_type.capitalize()} {self.name} [{self.service_tag}] ({warranty_text})'
+        if self.device_type == 'other':
+            device_type_text = ''
+        else:
+            device_type_text = f'{self.device_type.capitalize()} '
+
+        return f'{device_type_text}{self.name} [{self.service_tag}] ({warranty_text})'
+
+    def has_warranty(self) -> bool:
+        return self.warranty_type is not None
 
     def has_warranty_expired(self) -> bool:
-        return self.warranty_expiration_date < datetime.now()
+        if self.has_warranty():
+            return self.warranty_expiration_date < datetime.now()
+
+        return True
 
     def get_days_until_warranty_expires(self) -> int:
-        return (self.warranty_expiration_date - datetime.now()).days
+        if self.has_warranty():
+            return (self.warranty_expiration_date - datetime.now()).days
+
+        return 0
